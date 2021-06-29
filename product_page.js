3224d9514160c5233b074ace8710472978a492fa
Daniel Ryan <ryandaniel625@gmail.com>
2021-06-29 10:49:34 +0100

    modal working for enlarged image on product.html

--- product_page.js
+++ product_page.js
@@ -22,11 +22,32 @@ function buildImages() {
     fullImg.src = smallImg.src;
 }
 
-function enlargeImage(fullImg){
-    var enlargeImg = document.getElementById("enlargeImg");
-    var fullImg = document.getElementById("bigImg");
-    enlargeImg.src = fullImg.src;
-}
 
 var productNumber = localStorage.getItem("productIndex");
 console.log(productNumber);
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("bigImg");
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
