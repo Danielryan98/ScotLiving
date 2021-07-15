ebbd6f3c0678e6ad2ae81e916e35c5c9659e28a8
Daniel Ryan <ryandaniel625@gmail.com>
2021-07-15 11:07:40 +0100

    All pages updated with working links etc

--- index.js
+++ index.js
@@ -97,28 +97,3 @@ function getElementY(query) {
   document.getElementById('scrollTop').addEventListener('click', doScrolling.bind(null, '#top', 1500))
   document.getElementById('scrollBot').addEventListener('click', doScrolling.bind(null, '#bottom', 4000))}
 
-  // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
