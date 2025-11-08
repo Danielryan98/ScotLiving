57e48d95c191bd02b4357cdb39331c6e674f51be
DANIEL RYAN <ryandaniel625@gmail.com>
2025-11-08 21:17:21 +0000

    working on shopify link

---
+++ nav.inline.js
@@ -0,0 +1,31 @@
+document.write(`<div class="second-header-top">
+    <div class="logo">
+    <a href="index.html">      <img src="/Assets/Scot_Living_Logos/Logo/Standard Logo Files/logo.jpg"
+</a></div>
+    <!-- <div class="search-container">
+        <input class="search-input" type="text" placeholder="Search for products & inspiration">
+    </div> -->
+</div>
+<nav class="navbar navbar-expand-lg navbar-light navbar-custom py-0" style="background-color: #304ca0">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Offers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="fabricsofas.html">Mattresses</a>
+        </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Bed Frames</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Bed Sets</a>
+          </li>
+      </ul>
+    </div>
+</nav>
+`);
